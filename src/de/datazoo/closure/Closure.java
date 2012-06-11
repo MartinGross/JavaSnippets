0a57e163af273523f64c8ee507f55b3b9f2e20a0
Martin Gross <martin.gross.sp@lhsystems.com>
2012-06-11 15:22:36 +0200

    hmmm

--- src/de/datazoo/closure/Closure.java
+++ src/de/datazoo/closure/Closure.java
@@ -14,6 +14,15 @@ public class Closure {
 
         worker.run();
 
+        Closure closure = new Closure();
+        closure.runClosure(worker);
+
+    }
+
+    public void runClosure(Runnable closure) {
+
+        closure.run();
+
     }
 
 }
